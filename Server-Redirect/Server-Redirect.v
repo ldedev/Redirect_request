6ea40f24cfbc93215394f78ef0cb918b6959d876
AndreLuiz <andre_jp97@hotmail.com>
2022-08-16 16:55:59 -0300

    again a new correction url

--- Server-Redirect/Server-Redirect.v
+++ Server-Redirect/Server-Redirect.v
@@ -67,8 +67,8 @@ fn (mut ws Ws) redirect_me_access(cnpj_cpf string) vweb.Result {
 					url_param[1..] or { '' }
 				}
 				url_param.starts_with('/:') {
-					value := url_param[2..] or { '' }
-					'/$value'
+					value := url_param[2..] or { '/' }
+					'$value'
 				}
 				else {
 					url_param
